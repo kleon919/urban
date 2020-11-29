08203fbfd7e3ce1889613b552ed9ef079f652ba9
Kleanthis <kleon919@gmail.com>
2020-11-29 21:14:31 +0200

    Retry accross multiple providers
    - Add functionality to retry in case of unsuccessfull response
    - Handle empty response and error as well
    - Refine dependencies
    - Minor fixes

--- index.js
+++ index.js
@@ -1,25 +1,46 @@
 require("dotenv").config();
-
 const express = require("express");
-// const bodyParser = require("body-parser");
+
+const providers = require('./geocoding-providers');
+const findArea = require('./polygons');
 
 const app = express();
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(bodyParser.json());
 
 const validate = (req, res, next) => {
     // todo Use joi here to validate the query string parameter -> address
     return next()
+};
+
+// Apply a recursive call to each existing provider into the array.
+// Responds with success ('OK' status) only when a valuable result has been returned.
+// In other case, proceeds to the next provider, if exists or responds with 'NOT_FOUND' status
+const retryAcrossProviders = async (providers, address) => {
+    const currentProvider = providers.shift();
+    let res = await currentProvider.request(address).catch(e => e);
+    if (res.ok) return res
+    return (providers.length > 0)
+        ? retryAcrossProviders(providers, address)
+        : res; // return predefined not find msg
 }
+
+const keepWhatYouNeed = res => res.results.reduce((acc, val) => {
+
+}, {})
 
 // Function to handle the info path
 app.get('/info', validate, async (req, res) => {
 
     // Access the provided 'address' query parameter
     let address = req.query.address;
-    let encoded = encodeURI(address);
 
-    return res.send(encoded)
+    try {
+        let result = await retryAcrossProviders([...providers], encodeURI(address));
+        let {name} = findArea([-0.10422, 51.53196]); // todo
+        return res.send({name, result});
+    } catch (e) {
+        console.log(e)
+        res.send(500)
+    }
 });
 
 app.listen(5000, () => {
